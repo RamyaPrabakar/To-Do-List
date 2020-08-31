a578322e8b4ab27c828b2f55b4026c0f7551a973
RamyaPrabakar <ramyaprabakar01@gmail.com>
2020-08-31 20:28:11 +0530

    beautified code using eslint

--- public/to-do-list.js
+++ public/to-do-list.js
@@ -1,115 +1,104 @@
+/* eslint-disable linebreak-style */
 const input = document.querySelector('.input');
 const addButton = document.querySelector('.addButton');
-var container = document.querySelector('.container');
+const container = document.querySelector('.container');
 
-
-function createDiv(taskName, task_ID){
-
-    let task = document.createElement('input');
-    task.value = taskName;
-    task.disabled = true;
-    task.classList.add('viewTask');
-    task.type = "text";
-
-    let edit = document.createElement('button');
-    edit.innerHTML = "EDIT";
-    edit.classList.add('editButton');
-
-    let remove = document.createElement('button');
-    remove.innerHTML = "REMOVE";
-    remove.classList.add('removeButton');
-
-    let taskBox = document.createElement('div');
-    taskBox.classList.add('viewStage');
-
-    let taskID = document.createElement('input');
-    taskID.value = task_ID;
-    taskID.disabled = true;
-    taskID.classList.add('hiddentext');
-
-    container.appendChild(taskBox);
-    taskBox.appendChild(taskID);
-    taskBox.appendChild(task);
-    taskBox.appendChild(edit);
-    taskBox.appendChild(remove);
-
-    edit.addEventListener('click', function(){
-        task.disabled = !task.disabled;
-
-        if(task.disabled == true){
-            editTaskInDB(task.value, task_ID);
-        }
-    });
-
-    remove.addEventListener('click', function(){
-        removeTaskFromDB(task_ID);
-        container.removeChild(taskBox);
-    });
-
-}
-
-function addTaskToDB(tName)
-{
-  var userRequest = new XMLHttpRequest();
-  userRequest.open('post', '/addTask');
-  userRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
-  userRequest.send(JSON.stringify({'taskName':tName}));
-
-  userRequest.onreadystatechange = function() {
-    if (userRequest.readyState == 4 && userRequest.status == 200)
-    {
-    //console.log(userRequest.responseText);
-    var text = (userRequest.responseText).toString();
-    var pj = JSON.parse(text);
-    console.log(pj[0].task_id);
-    createDiv(tName, pj[0].task_id);
-    }
-}
-
-}
-
-function editTaskInDB(tName, tID)
-{
-  console.log("Came to editTaskInDB");
+function editTaskInDB(tName, tID) {
   const userRequest = new XMLHttpRequest();
   userRequest.open('post', '/editTask');
-  userRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
-  userRequest.send(JSON.stringify({'taskName':tName , 'taskID':tID}));
-
+  userRequest.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  userRequest.send(JSON.stringify({ taskName: tName, taskID: tID }));
 }
 
-function removeTaskFromDB(task_id)
-{
+function removeTaskFromDB(tId) {
   const userRequest = new XMLHttpRequest();
   userRequest.open('post', '/removeTask');
-  userRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
-  userRequest.send(JSON.stringify({'taskID':task_id}));
+  userRequest.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  userRequest.send(JSON.stringify({ taskID: tId }));
 }
 
-function getTasks(){
-    var Httpreq = new XMLHttpRequest();
-    Httpreq.open("GET",'/getTasks',false);
-    Httpreq.send(null);
-    return Httpreq.responseText;
-}
+function createDiv(taskName, tID) {
+  const task = document.createElement('input');
+  task.value = taskName;
+  task.disabled = true;
+  task.classList.add('viewTask');
+  task.type = 'text';
 
- function verify(){
-    var str = input.value.trim();
-    if(str != ""){
-       addTaskToDB(str);
-       input.value = "";
+  const edit = document.createElement('button');
+  edit.innerHTML = 'EDIT';
+  edit.classList.add('editButton');
+
+  const remove = document.createElement('button');
+  remove.innerHTML = 'REMOVE';
+  remove.classList.add('removeButton');
+
+  const taskBox = document.createElement('div');
+  taskBox.classList.add('viewStage');
+
+  const taskID = document.createElement('input');
+  taskID.value = tID;
+  taskID.disabled = true;
+  taskID.classList.add('hiddentext');
+
+  container.appendChild(taskBox);
+  taskBox.appendChild(taskID);
+  taskBox.appendChild(task);
+  taskBox.appendChild(edit);
+  taskBox.appendChild(remove);
+
+  edit.addEventListener('click', () => {
+    task.disabled = !task.disabled;
+
+    if (task.disabled === true) {
+      editTaskInDB(task.value, tID);
     }
+  });
+
+  remove.addEventListener('click', () => {
+    removeTaskFromDB(tID);
+    container.removeChild(taskBox);
+  });
 }
 
-function main(){
-    addButton.addEventListener('click', verify);
-    document.addEventListener("DOMContentLoaded", function() {
-        var json_obj = JSON.parse(getTasks());
+function addTaskToDB(tName) {
+  const userRequest = new XMLHttpRequest();
+  userRequest.open('post', '/addTask');
+  userRequest.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  userRequest.send(JSON.stringify({ taskName: tName }));
 
-        json_obj.forEach((item) => {
-            createDiv(item.task_name, item.task_id);
-        });
-});
+  userRequest.onreadystatechange = () => {
+    if (userRequest.readyState === 4 && userRequest.status === 200) {
+      const text = (userRequest.responseText).toString();
+      const pj = JSON.parse(text);
+      createDiv(tName, pj[0].task_id);
+    }
+  };
+}
+
+function getTasks() {
+  const Httpreq = new XMLHttpRequest();
+  Httpreq.open('GET', '/getTasks', false);
+  Httpreq.send(null);
+  return Httpreq.responseText;
+}
+
+function verify() {
+  const str = input.value.trim();
+  if (str !== '') {
+    addTaskToDB(str);
+    input.value = '';
+  }
+}
+
+function main() {
+  addButton.addEventListener('click', verify);
+  document.addEventListener('DOMContentLoaded', () => {
+    const jsonObj = JSON.parse(getTasks());
+
+    jsonObj.forEach((item) => {
+      createDiv(item.task_name, item.task_id);
+    });
+  });
 }
 
 main();
--- todoserver.js
+++ todoserver.js
@@ -1,84 +1,79 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const path = require('path');
-var connected = false;
-var jsonParser = bodyParser.json();
 
-var app = express();
+let connected = false;
+const jsonParser = bodyParser.json();
+
+const app = express();
 app.use(bodyParser.json());
 
-var mysqlConnection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'tribias4',
-    database : 'ToDoListDB',
-    multipleStatements : true
+const mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'tribias4',
+  database: 'ToDoListDB',
+  multipleStatements: true,
 });
 
 mysqlConnection.connect((err) => {
-    if(err){
-        console.log('Connection Failed');
-    } else {
-        console.log('Connected');
-        connected = true;
-    }
-})
-
-app.post('/addTask', jsonParser, function(req, res) {
-
-  if (connected) {
-    var sql1 ='INSERT INTO todolist (task_name) VALUES ("'+req.body.taskName+'")';
-    mysqlConnection.query(sql1, function(err) {
-        if(err) throw err;
-    });
-    let sql2 = 'SELECT LAST_INSERT_ID() as task_id';
-    mysqlConnection.query(sql2, function(err, results) {
-        if(err) throw err;
-        console.log(results);
-        res.send(results);
-    });
-
+  if (err) {
+    console.log('Connection Failed');
+  } else {
+    console.log('Connected');
+    connected = true;
   }
-})
+});
 
-app.post('/removeTask', jsonParser, function(req, res) {
-
+app.post('/addTask', jsonParser, (req, res) => {
   if (connected) {
-    var sql3 = "DELETE FROM todolist WHERE task_id ="+ req.body.taskID;
-    console.log(sql3);
-    mysqlConnection.query(sql3, function(err) {
-        if(err) throw err;
+    const sql1 = `INSERT INTO todolist (task_name) VALUES ("${req.body.taskName}")`;
+    mysqlConnection.query(sql1, (err) => {
+      if (err) throw err;
+    });
+    const sql2 = 'SELECT LAST_INSERT_ID() as task_id';
+    mysqlConnection.query(sql2, (err, results) => {
+      if (err) throw err;
+      res.send(results);
     });
   }
-})
+});
 
-app.post('/editTask', jsonParser, function(req, res) {
-
+app.post('/removeTask', jsonParser, (req) => {
   if (connected) {
-    var sql4 = "UPDATE todolist SET task_name = '" + req.body.taskName + "' WHERE task_id = "+ req.body.taskID;
-    console.log(sql4);
-    mysqlConnection.query(sql4, function(err) {
-        if(err) throw err;
+    const sql3 = `DELETE FROM todolist WHERE task_id =${req.body.taskID}`;
+    mysqlConnection.query(sql3, (err) => {
+      if (err) throw err;
     });
   }
-})
+});
+
+app.post('/editTask', jsonParser, (req) => {
+  if (connected) {
+    const sql4 = `UPDATE todolist SET task_name = '${req.body.taskName}' WHERE task_id = ${req.body.taskID}`;
+    mysqlConnection.query(sql4, (err) => {
+      if (err) throw err;
+    });
+  }
+});
 
 app.get('/getTasks', (req, res) => {
-  let sql = 'SELECT * FROM todolist';
+  const sql = 'SELECT * FROM todolist';
   mysqlConnection.query(sql, (err, results) => {
     if (err) {
       throw err;
     }
-    console.log(results);
     res.send(results);
-  })
-})
+  });
+});
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static('public'));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(3000);
